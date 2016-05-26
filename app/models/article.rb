6cabc430c158029b02830579560e5a051f064351
isoshii <ubuntubak@gmail.com>
2016-05-26 15:59:02 +0000

    pierwsze wysłanie

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ActiveRecord::Base
+    has_many :comments, dependent: :destroy
+    validates :title, presence: true,
+                    length: { minimum: 3 }
+    validates :text, presence: true,
+                    length: { minimum: 6 }
+end
